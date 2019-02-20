eb5cfbc7ed59bff7b4236e5009585a45f5d2fcb0
jeffswt <jeffswt@users.noreply.github.com>
2019-02-20 16:41:50 +0800

    Implement login method.

--- elective.py
+++ elective.py
@@ -1,4 +1,5 @@
 
+import json
 import requests
 
 
@@ -6,10 +7,33 @@ class WebSession:
     def __init__(self):
         self.cookies_jar = requests.cookies.RequestsCookieJar()
         return
-    
+
     def get(self, url, **kwargs):
         return requests.get(url, cookies=self.cookies_jar, **kwargs)
-    
+
     def post(self, url, **kwargs):
         return requests.post(url, cookies=self.cookies_jar, **kwargs)
+    pass
+
+
+class UserSession:
+    """Manages user and related information."""
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.session = WebSession()
+        return
+
+    def login(self):
+        url = 'https://v.ruc.edu.cn/auth/login'
+        payload = {
+            "username": "ruc:%s" % self.username,
+            "password": "%s" % self.password,
+            "remember_me": "false",
+            "redirect_uri": "https%3A%2F%2Fv.ruc.edu.cn%2Foauth2%2Fauthorize",
+            "twofactor_password": "",
+            "twofactor_recovery": ""
+        }
+        r = self.session.get(url, data=json.dumps(payload))
+        return r.text
     pass
